1cefac143622b3e2b704d4f95c9ff106dcf697c0
Amit Phaltankar <amit.phaltankar@objective.com>
2021-07-26 10:22:59 +1000

    map,sorting,streams

--- java-streams/src/main/java/com/amitph/java/streams/model/Student.java
+++ java-streams/src/main/java/com/amitph/java/streams/model/Student.java
@@ -3,14 +3,9 @@ package com.amitph.java.streams.model;
 import lombok.Data;
 
 @Data
-public class Student implements Comparable<Student> {
+public class Student {
     private final Long studentId;
     private final String firstName;
     private final String lastName;
     private final Integer age;
-
-    @Override
-    public int compareTo(Student student) {
-        return this.firstName.compareTo(student.firstName);
-    }
 }
