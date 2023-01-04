c9de715ec693abcda91e689d1552d7d60cedb5f1
Amit Phaltankar <amitrp.java@gmail.com>
2023-01-04 17:18:21 +1100

    aph-7208 inputstream to byte[]

---
+++ java-io/src/main/java/com/amitph/java/io/InputStreamToByteArray.java
@@ -0,0 +1,70 @@
+package com.amitph.java.io;
+
+import com.google.common.io.ByteStreams;
+import org.apache.commons.io.IOUtils;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+public class InputStreamToByteArray {
+    public InputStream toInputStream(byte[] bytes) {
+        return new ByteArrayInputStream(bytes);
+    }
+
+    public byte[] toByteArrayUsingPlainJava(InputStream inputStream) throws IOException {
+        int size = inputStream.available();
+        byte[] bytes = new byte[size];
+        inputStream.read(bytes);
+        return bytes;
+    }
+
+    public byte[] toByteArrayManually(InputStream inputStream) throws IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+        byte[] bucket = new byte[1024];
+        int nReadBytes;
+        while ((nReadBytes = inputStream
+                .read(bucket, 0, bucket.length)) != -1) {
+            byteArrayOutputStream.write(bucket, 0, nReadBytes);
+        }
+
+        return byteArrayOutputStream.toByteArray();
+    }
+
+    public byte[] toByteArrayUsingApacheCommons(InputStream inputStream) throws IOException {
+        return IOUtils.toByteArray(inputStream);
+    }
+
+    public byte[] toByteArrayUsingGuava(InputStream inputStream) throws IOException {
+        return ByteStreams.toByteArray(inputStream);
+    }
+
+
+    public static void main(String[] a) throws IOException {
+        InputStreamToByteArray converter = new InputStreamToByteArray();
+        byte[] input = """
+                "this sample text that we will convert
+                to a byte[] then to InputStream"
+                """.getBytes(UTF_8);
+
+        InputStream inputStream1 = converter.toInputStream(input);
+        System.out.println("InputStream to byte[] using plain Java");
+        System.out.println(new String(converter.toByteArrayUsingPlainJava(inputStream1)));
+
+        InputStream inputStream2 = converter.toInputStream(input);
+        System.out.println("InputStream to byte[] using plain Manually");
+        System.out.println(new String(converter.toByteArrayManually(inputStream2)));
+
+        InputStream inputStream3 = converter.toInputStream(input);
+        System.out.println("InputStream to byte[] using Apache Commons IO");
+        System.out.println(new String(converter.toByteArrayUsingApacheCommons(inputStream3)));
+
+        InputStream inputStream4 = converter.toInputStream(input);
+        System.out.println("InputStream to byte[] using plain Guava");
+        System.out.println(new String(converter.toByteArrayUsingGuava(inputStream4)));
+    }
+}
